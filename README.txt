4381293b3e6ca320d2fcb7a1d9d20bc4f0b7e142
jlxc2001 <jlxc2001@outlook.com>
2026-07-16 05:13:19 +0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+JLHUD MVP v1.0.2 patch
+
+Fix:
+MainActivity references JLHudApp correctly.
+
+Replace:
+app/src/main/java/com/jlxc/jlhud/MainActivity.kt
--- app/src/main/java/com/jlxc/jlhud/MainActivity.kt
+++ app/src/main/java/com/jlxc/jlhud/MainActivity.kt
@@ -6,17 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.jlxc.jlhud.JLHudApp
 
 class MainActivity: ComponentActivity(){
- override fun onCreate(savedInstanceState: Bundle?){
-  super.onCreate(savedInstanceState)
 
-  WindowCompat.setDecorFitsSystemWindows(window,false)
-  WindowInsetsControllerCompat(window,window.decorView)
-   .hide(WindowInsetsCompat.Type.systemBars())
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
 
-  setContent{
-   JLHudApp()
-  }
- }
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+
+        WindowInsetsControllerCompat(
+            window,
+            window.decorView
+        ).hide(
+            WindowInsetsCompat.Type.systemBars()
+        )
+
+        setContent {
+            JLHudApp()
+        }
+    }
 }
